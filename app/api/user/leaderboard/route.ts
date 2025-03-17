678f1173ee35d8a4f828da4da5babc4a67c7ff28
developerdeepak25 <developer.deepak25@gmail.com>
2025-03-17 15:33:48 +0530

    auth completed 1

---
+++ app/api/user/leaderboard/route.ts
@@ -0,0 +1,63 @@
+export const revalidate = 10 * 60; // 10 minutes
+
+import { PrismaClient } from "@prisma/client";
+import { type NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+/**
+ * * considerations
+ * - cache  --- //* DONE
+ * - rate limit
+ *
+ */
+export async function GET(request: NextRequest) {
+  try {
+    //! commented this just authorization code for testing
+    // const session = await getServerSession(authConfig);
+    // console.log(session);
+    // if (!session)
+    //   return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+    const searchParams = request.nextUrl.searchParams;
+    const page = parseInt(searchParams.get("page") || "1");
+    const limit = parseInt(searchParams.get("limit") || "10");
+    const skip = (page - 1) * limit;
+
+    const users = await prisma.user.findMany({
+      orderBy: {
+        jpEarned: "desc",
+      },
+      // include: {
+      // },
+      omit: {
+        password: true,
+      },
+      take: limit,
+      skip: skip,
+      
+    });
+    console.log(users); //?dev
+    if (!users) {
+      return NextResponse.json({ message: "No users found" }, { status: 404 });
+    }
+    const updatedUsers = users.map((user) => {
+      return {
+        ...user,
+        jpTransaction: user.jpEarned + user.jpSpent,
+        jpBalance: user.jpEarned - user.jpSpent,
+      };
+    });
+
+    return NextResponse.json(
+      { users: updatedUsers, message: "success", page, limit }, // Fixed typo from "sucess"
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+}
