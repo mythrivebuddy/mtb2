75d7ff5000015022700dab95c82d7b21b9469c84
Ghanishth12 <solankilavesh1202@gmail.com>
2025-03-31 17:19:18 +0530

    final changes in next config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: process.env.SUPABASE_HOST_URL,
+        hostname: process.env.SUPABASE_HOST_URL ?? "", // Ensure a string
       },
     ],
   },
   experimental: {
-    serverActions: true,
+    serverActions: {},
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
