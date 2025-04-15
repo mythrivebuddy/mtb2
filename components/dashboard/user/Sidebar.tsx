fcde19dc39fda49729385d172cf88494484c7063
Faizan998 <alifaizan15245@gmail.com>
2025-04-15 12:35:40 +0530

    update contact-us from and fix bugs

--- components/dashboard/user/Sidebar.tsx
+++ components/dashboard/user/Sidebar.tsx
@@ -125,7 +125,7 @@ const Sidebar = ({ user }: { user?: UserType }) => {
             <NavItem
               href="/dashboard/progress-vault"
               icon={<Vault size={20} />}
-              label="1 % Progress-Vault" />
+              label="1%Progress Vault" />
 
             <NavItem
               href="/dashboard/prosperity"
@@ -164,7 +164,7 @@ const Sidebar = ({ user }: { user?: UserType }) => {
               label="FAQ's"
             />
             <NavItem
-              href="/dashboard/contact"
+              href="/contact"
               icon={<Phone size={20} />}
               label="Contact us"
             />
