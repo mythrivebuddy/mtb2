e2fee74258bc714f1e05bd3050f6ce46bd7dc555
developerdeepak25 <developer.deepak25@gmail.com>
2025-04-29 19:08:42 +0530

    bug fix suggested on 28-29 march

--- components/aligned-actions/AlignedActionWizard.tsx
+++ components/aligned-actions/AlignedActionWizard.tsx
@@ -673,9 +673,9 @@ export default function AlignedActionWizard({
                               const date = timeToDate(e.target.value);
                               if (date) field.onChange(date);
                             }}
-                            className="pl-10"
+                            // className="pl-10"
                           />
-                          <svg
+                          {/* <svg
                             className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500"
                             fill="none"
                             viewBox="0 0 24 24"
@@ -687,7 +687,7 @@ export default function AlignedActionWizard({
                               strokeWidth={2}
                               d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
                             />
-                          </svg>
+                          </svg> */}
                         </div>
                       </FormControl>
                       <FormMessage />
@@ -710,9 +710,9 @@ export default function AlignedActionWizard({
                               const date = timeToDate(e.target.value);
                               if (date) field.onChange(date);
                             }}
-                            className="pl-10"
+                            // className="pl-10"
                           />
-                          <svg
+                          {/* <svg
                             className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500"
                             fill="none"
                             viewBox="0 0 24 24"
@@ -724,7 +724,7 @@ export default function AlignedActionWizard({
                               strokeWidth={2}
                               d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
                             />
-                          </svg>
+                          </svg> */}
                         </div>
                       </FormControl>
                       <FormMessage />
