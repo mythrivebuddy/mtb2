fd1d9ad8dcd767fdee21cd6985338697622f0693
gouravN0709 <gourav.narvariya@vkaps.com>
2025-06-11 09:32:58 +0530

    fixed packages

--- app/api/blogs/getParticularBlog/[id]/route.ts
+++ app/api/blogs/getParticularBlog/[id]/route.ts
@@ -7,7 +7,7 @@ export async function GET(
 ) {
   const { id } = await params;
   const pureId = id.split("-")[0];
-  console.log('pureId', pureId)
+ 
   try {
     const blog = await prisma.blog.findUnique({
       where: {
