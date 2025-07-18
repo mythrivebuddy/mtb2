a0f8b6c474ba07b8a367b06e77b4201c79169550
Toheed <qureshitoheed098@gmail.com>
2025-07-18 17:36:25 +0530

    Fixign

--- app/(public)/survey/question-page/[questionId]/page.tsx
+++ app/(public)/survey/question-page/[questionId]/page.tsx
@@ -3,9 +3,10 @@ import AppLayout from "@/components/layout/AppLayout";
 import QuestionPageComponent from "@/components/survey/QuestionPage";
 import { useParams } from "next/navigation";
 
+
 export default function QuestionPage() {
-    const params = useParams();
-    const questionId = params.questionId as string;
+   const params = useParams();
+   const questionId= params.questionId as string;
   return (
     <AppLayout>
       <QuestionPageComponent questionId={questionId} />
