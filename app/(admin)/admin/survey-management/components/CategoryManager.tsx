c96deab7c79bf0614e655aa886c48ae2cdd1a5cb
Kartik Silodiya <71602964+Kirito16-06@users.noreply.github.com>
2025-09-18 16:06:07 +0530

    check

--- app/(admin)/admin/survey-management/components/CategoryManager.tsx
+++ app/(admin)/admin/survey-management/components/CategoryManager.tsx
@@ -1,5 +1,42 @@
 'use client';
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { useState, ChangeEvent } from 'react';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
