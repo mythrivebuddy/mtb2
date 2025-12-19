260e8f35b2f54c8389de01517450c242faea879f
Toheed Qureshi <qureshitoheed098@gmail.com>
2025-12-19 18:47:59 +0530

    optimized the user challenges

--- providers/Provider.tsx
+++ providers/Provider.tsx
@@ -3,20 +3,30 @@
 import { Toaster } from "@/components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
-import React from "react";
-
-const queryClient = new QueryClient();
+import React, { useState } from "react";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+            refetchOnMount: false,
+          },            
+        },
+      })
+  );
+
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <SessionProvider>
-          {children}
-          <Toaster />
-        </SessionProvider>
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>
+        {children}
+        <Toaster />
+      </SessionProvider>
+    </QueryClientProvider>
   );
 };
 
