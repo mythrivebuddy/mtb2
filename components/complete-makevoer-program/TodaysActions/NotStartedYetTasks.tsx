0a116ee78d9b832f98121988567217e9269d5cf5
Toheed Qureshi <qureshitoheed098@gmail.com>
2026-01-06 16:52:58 +0530

    fix build issue

--- components/complete-makevoer-program/TodaysActions/NotStartedYetTasks.tsx
+++ components/complete-makevoer-program/TodaysActions/NotStartedYetTasks.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Hourglass, ArrowRight } from "lucide-react";
+import { Hourglass } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 /* ---------------- Types ---------------- */
