83ac742dbed0dca7ec1590fa9c76639fc52c3399
Toheed Qureshi <qureshitoheed098@gmail.com>
2026-01-12 16:37:14 +0530

    fixing ui things and validations on the today's focus page

--- components/complete-makevoer-program/TodaysActions/PaginationIndicatorsDailyActions.tsx
+++ components/complete-makevoer-program/TodaysActions/PaginationIndicatorsDailyActions.tsx
@@ -5,22 +5,25 @@ export default function PaginationIndicatorsDailyActions({
   commitments,
   setCurrentSlideIndex,
   currentSlideIndex,
+  disabled
 }: {
   commitments: Commitment[];
   setCurrentSlideIndex: React.Dispatch<React.SetStateAction<number>>;
   currentSlideIndex: number;
+  disabled?:boolean
 }) {
   return (
     <div className="flex items-center justify-center gap-3 mb-8">
       {commitments?.map((_, index) => (
         <button
           key={index}
+          disabled={disabled}
           onClick={() => setCurrentSlideIndex(index)}
           aria-label={`Go to slide ${index + 1}`}
           className={`h-2 rounded-full transition-all ${
             index === currentSlideIndex
               ? "w-8 bg-[#1990e6]"
-              : "w-2 bg-slate-300 hover:bg-slate-400"
+              : "w-2 bg-slate-300 hover:bg-slate-400 disabled:hover:cursor-not-allowed"
           }`}
         />
       ))}
