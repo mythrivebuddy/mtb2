f6c43584b0d0e53726f64c2be3265c53750d6f31
Toheed Qureshi <qureshitoheed098@gmail.com>
2026-03-10 14:00:46 +0530

    Fixes

--- app/api/user/history/route.ts
+++ app/api/user/history/route.ts
@@ -65,7 +65,8 @@ export async function GET(request: Request) {
         })
         : Promise.resolve([]);
     const paymentOrdersPromise =
-      filter === "ALL" || filter === "SUBSCRIPTION" || filter === "STORE_ORDER"
+      filter === "ALL" || filter === "SUBSCRIPTION" 
+      // || filter === "STORE_ORDER"
         ? prisma.paymentOrder.findMany({
           where: {
             userId,
